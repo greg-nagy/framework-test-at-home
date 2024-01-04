adbe666b8ca13f5a6c71fc213bca9b4447c276d5
Greg Nagy <greg@dhamma.works>
2024-01-04 22:47:02 +0100

    asdf

--- rust_axum_tokio-postgres-bb8_optimized/src/main.rs
+++ rust_axum_tokio-postgres-bb8_optimized/src/main.rs
@@ -5,8 +5,11 @@ use axum::{
     Router,
 };
 
+use std::{net::{SocketAddr, Ipv4Addr}, io};
+use tokio::net::{TcpListener, TcpSocket};
+
 mod database;
-mod server;
+// mod server;
 
 use self::database::{DatabaseConnection, PgConnection};
 
@@ -33,6 +36,20 @@ fn main() {
         });
     }
     rt.block_on(serve());
+
+// let rt = tokio::runtime::Builder::new_multi_thread()
+//     .worker_threads(10) // specify the number of threads here
+//     .enable_all()
+//     .build()
+//     .unwrap();    
+
+    // for _ in 0..10 {
+    //     rt.spawn(serve());
+    // }
+
+    // // Keep the main thread alive until all tasks are complete
+    // rt.block_on(serve());
+
 }
 
 async fn serve() {
@@ -41,14 +58,33 @@ async fn serve() {
     // setup connection pool
     let pg_connection = PgConnection::connect(database_url).await;
 
-    let router = Router::new()
+    let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/count", get(count))
         .with_state(pg_connection);
 
-    server::builder()
-        .serve(router.into_make_service())
-        .await
-        .unwrap();
+    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
+    let listener = reuse_listener(addr).expect("couldn't bind to addr");
+
+    println!("Started axum server at 3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
 
+fn reuse_listener(addr: SocketAddr) -> io::Result<TcpListener> {
+    let socket = match addr {
+        SocketAddr::V4(_) => TcpSocket::new_v4()?,
+        SocketAddr::V6(_) => TcpSocket::new_v6()?,
+    };
+
+    #[cfg(unix)]
+    {
+        if let Err(e) = socket.set_reuseport(true) {
+            eprintln!("error setting SO_REUSEPORT: {e}");
+        }
+    }
+
+    socket.set_reuseaddr(true)?;
+    socket.bind(addr)?;
+    socket.listen(1024)
+}
