213d2963157e9c24a349aa321b0c156fe63e296e
Greg Nagy <greg@dhamma.works>
2024-01-06 14:50:17 +0100

    add 10 thread and

---
+++ rust_axum_tokio-postgres/src/main-10_thread.rs
@@ -0,0 +1,49 @@
+use std::env;
+
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+
+mod database;
+
+use self::database::{DatabaseConnection, PgConnection};
+
+async fn count(DatabaseConnection(conn): DatabaseConnection) -> impl IntoResponse {
+    let count = conn.get_count().await.expect("error loading world");
+
+    (StatusCode::OK, count)
+}
+
+
+fn main() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(10) // specify the number of threads here
+        .enable_all()
+        .build()
+        .unwrap();    
+
+    rt.block_on(serve());
+}
+
+async fn serve() {
+    let db_url = match env::var("DB_URL") {
+        Ok(value) => value,
+        Err(e) => {
+            println!("Couldn't read MY_ENV_VAR ({})", e);
+            return; // or handle the error as needed
+        },
+    };
+
+    let pg_connection = PgConnection::connect(db_url).await;
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/count", get(count))
+        .with_state(pg_connection);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
