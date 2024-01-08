8ffed6aaee10d4c02feffdbfcca797f0df23acdd
Greg Nagy <greg@dhamma.works>
2024-01-08 11:06:29 +0100

    udpate bb8 impl to listen on 0.0.0.0

--- rust_axum_tokio-postgres-bb8/src/main.rs
+++ rust_axum_tokio-postgres-bb8/src/main.rs
@@ -26,7 +26,7 @@ async fn main() {
         .with_state(pool);
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
