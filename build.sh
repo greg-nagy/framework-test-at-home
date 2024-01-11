1eb7d9ea62a528ca018cbf7c80717e6d5ce1c5f1
Greg Nagy <greg@dhamma.works>
2024-01-11 10:02:37 +0100

    add build script

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-cd rust_axum_tokio-postgres
+# Define the array of names
+names=(
+    "rust_axum_tokio-postgres"
+    "rust_axum_tokio-postgres_bb8"
+    "rust_axum_tokio-postgres_prefork"
+    "rust_axum_tokio-postgres_tech-empower"
+)
+
+# Iterate over each name
+for name in "${names[@]}"; do
+    # Change to the directory
+    cd "$name" || exit
+
+    # Build the project
+    RUSTFLAGS="-C target-cpu=native" cargo build --release
+
+    # Copy the binary to the parent directory
+    cp "./target/release/$name" ../
+
+    # Go back to the original directory
+    cd ..
+
+    echo "Processed $name"
+done
+
+echo "All processes completed."
