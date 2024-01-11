cd4352377377b89846a4d7ab98c53b733dde5a50
Greg Nagy <greg@dhamma.works>
2024-01-11 09:40:10 +0100

    add rust axum prefork

---
+++ rust_axum_tokio-potsgres_prefork/src/database.rs
@@ -0,0 +1,77 @@
+use std::{convert::Infallible, io, sync::Arc};
+
+use axum::{async_trait, extract::FromRequestParts, http::request::Parts};
+
+use tokio_postgres::{connect, Client, NoTls, Statement};
+
+
+#[derive(Debug)]
+pub enum PgError {
+    Io(io::Error),
+    Pg(tokio_postgres::Error),
+}
+
+impl From<io::Error> for PgError {
+    fn from(err: io::Error) -> Self {
+        PgError::Io(err)
+    }
+}
+
+impl From<tokio_postgres::Error> for PgError {
+    fn from(err: tokio_postgres::Error) -> Self {
+        PgError::Pg(err)
+    }
+}
+
+/// Postgres interface
+pub struct PgConnection {
+    client: Client,
+    count: Statement,
+}
+
+impl PgConnection {
+    pub async fn connect(db_url: String) -> Arc<PgConnection> {
+        let (cl, conn) = connect(&db_url, NoTls)
+            .await
+            .expect("can not connect to postgresql");
+
+        // Spawn connection
+        tokio::spawn(async move {
+            if let Err(error) = conn.await {
+                eprintln!("Connection error: {error}");
+            }
+        });
+
+
+        let query = "SELECT count FROM presence_counters WHERE name = 'group_sittings' ORDER BY updated_at DESC LIMIT 1";
+        let count = cl.prepare(query).await.unwrap();
+
+        Arc::new(PgConnection {
+            client: cl,
+            count,
+        })
+    }
+}
+
+impl PgConnection {
+    pub async fn get_count(&self) -> Result<String, PgError> {
+        let row = self.client.query_one(&self.count, &[]).await?;
+        let value: i32 = row.get(0);
+        Ok(value.to_string())
+    }
+}
+
+pub struct DatabaseConnection(pub Arc<PgConnection>);
+
+#[async_trait]
+impl FromRequestParts<Arc<PgConnection>> for DatabaseConnection {
+    type Rejection = Infallible;
+
+    async fn from_request_parts(
+        _parts: &mut Parts,
+        pg_connection: &Arc<PgConnection>,
+    ) -> Result<Self, Self::Rejection> {
+        Ok(Self(pg_connection.clone()))
+    }
+}
+
---
+++ rust_axum_tokio-potsgres_prefork/src/main.rs
@@ -0,0 +1,80 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+
+
+use std::{env, net::{SocketAddr, Ipv4Addr}, io};
+use tokio::net::{TcpListener, TcpSocket};
+
+mod database;
+
+use self::database::{DatabaseConnection, PgConnection};
+
+async fn count(DatabaseConnection(conn): DatabaseConnection) -> impl IntoResponse {
+    let count = conn.get_count().await.expect("error loading world");
+
+    (StatusCode::OK, count)
+}
+
+fn main() {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    for _ in 1..10 {
+        std::thread::spawn(move || {
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+            rt.block_on(serve());
+        });
+    }
+    rt.block_on(serve());
+}
+
+async fn serve() {
+    let db_url = match env::var("DB_URL") {
+        Ok(value) => value,
+        Err(e) => {
+            println!("Couldn't read DB_URL ({})", e);
+            return; // or handle the error as needed
+        },
+    };
+
+    let pg_connection = PgConnection::connect(db_url).await;
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/count", get(count))
+        .with_state(pg_connection);
+
+    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3002));
+    let listener = reuse_listener(addr).expect("couldn't bind to addr");
+
+    println!("Started axum server at 3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn reuse_listener(addr: SocketAddr) -> io::Result<TcpListener> {
+    let socket = match addr {
+        SocketAddr::V4(_) => TcpSocket::new_v4()?,
+        SocketAddr::V6(_) => TcpSocket::new_v6()?,
+    };
+
+    #[cfg(unix)]
+    {
+        if let Err(e) = socket.set_reuseport(true) {
+            eprintln!("error setting SO_REUSEPORT: {e}");
+        }
+    }
+
+    socket.set_reuseaddr(true)?;
+    socket.bind(addr)?;
+    socket.listen(1024)
+}
